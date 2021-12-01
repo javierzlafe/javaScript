dc65dc22f228586c3920ec3ecb4237fe663bae94
Gaston Salomon <gastonsalomon17@gmail.com>
2021-12-01 01:28:40 -0300

    Septimo desafio Dom

---
+++ dom.js
@@ -0,0 +1,13 @@
+let h1=document.getElementById("titulo");
+console.log(h1.innerHTML="jaja");
+
+let h2=document.getElementById("subTitulo");
+h2.style.color="white";
+h2.style.background="orange";
+
+let botonSaludo=document.getElementById("boton");
+botonSaludo.style.background="black";
+botonSaludo.style.height="150px";
+botonSaludo.style.width="300px";
+botonSaludo.style.color="white";
+botonSaludo.style.fontSize="40px";
